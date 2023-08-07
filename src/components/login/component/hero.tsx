5c9824938eaf161bc8448eff9968d8a5ec65ee86
davisLjr <davisprojectjr@gmail.com>
2023-08-06 21:08:04 -0300

    fix: primera version del login

--- src/components/login/component/hero.tsx
+++ src/components/login/component/hero.tsx
@@ -1,19 +1,49 @@
-import React from "react"
-import { Box, Flex, Text, Button, Heading } from '@chakra-ui/react'
+import React, { ReactNode } from "react";
+import { Box, Flex, Text, Button, Heading } from "@chakra-ui/react";
 
-const Hero = () => {
+interface HeroProps {
+  children?: ReactNode;
+}
 
+const Hero: React.FC<HeroProps> = ({ children }) => {
   return (
-    <Flex>
-      <Box>
-        <Heading>Metodo Ahorro</Heading>
-
+    <Flex h="100vh">
+      <Box
+        w={{base:"100%", lg:"50%"}}
+        p="4rem 2rem 2rem"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        alignItems="center"
+      >
         <Box>
-          <Heading>Bienvenido de nuevo.</Heading>
-          <Text>Por favor inicia tu sesión.</Text>
+          <Heading textAlign="center" fontWeight="light" as="h2" size="xl">
+            Metodo Ahorro
+          </Heading>
+        </Box>
+        <Flex flexDirection="column" gap="3" textAlign="center">
+          <Heading fontWeight="medium" as="h3" size="xl" marginBottom="0">
+            Bienvenido.
+          </Heading>
+          <Text color="#a1a1a1">Por favor inicia tu sesión.</Text>
+          {children}
+        </Flex>
+        <Box>
+          <Text fontSize="sm" textAlign="center" color="#a1a1a1">
+            Con Método Ahorro, podrás gestionar tus finanzas de manera efectiva,
+            considerando tus activos y pasivos. También podrás establecer metas
+            de ahorro mensuales y lograr tus objetivos económicos personales.
+          </Text>
         </Box>
       </Box>
-      <Box backgroundImage="url('/login/login-hero.jpg')" w="50%" h="100vh"></Box>
+      <Box
+        w="50%"
+        display={{base: "none", lg: "block"}}
+        bgImage="url('/login/login-hero-two.jpg')"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        backgroundSize="cover"
+      />
     </Flex>
   );
 };
--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,10 +1,10 @@
 // login/index.tsx
 "use client";
 
-import React, { useEffect } from 'react';
-import netlifyIdentity from 'netlify-identity-widget';
-import { Button, Heading } from '@chakra-ui/react';
-import Hero from './component/hero';
+import React, { useEffect } from "react";
+import netlifyIdentity from "netlify-identity-widget";
+import { Button, Heading } from "@chakra-ui/react";
+import Hero from "./component/hero";
 
 const LoginPage = () => {
   useEffect(() => {
@@ -21,8 +21,25 @@ const LoginPage = () => {
 
   return (
     <div>
-      <Hero/>
-      <Button onClick={handleLogin}>Iniciar sesión con Netlify</Button>
+      <Hero>
+        <Button
+          background="black"
+          color="white"
+          fontWeight="light"
+          letterSpacing="2px"
+          onClick={handleLogin}
+          transition= "0.4s ease-out"
+          _hover={{
+            background: "#ffffff",
+            color: "black",
+            border: "2px solid black",
+            fontWeight: "medium",
+            transition: "0.4s ease-out",
+          }}
+        >
+          Continuar
+        </Button>
+      </Hero>
     </div>
   );
 };
